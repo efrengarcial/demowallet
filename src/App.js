45e9535971638db60ad03daba4236e48313f0117
Efren Garcia <efren.gl@gmail.com>
2022-05-05 11:40:15 -0500

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import BN from 'bn.js';
-import { Transaction } from '@metaplex-foundation/mpl-core';
 import { getPhantomWallet as PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { Connection,  actions } from '@metaplex/js';
-import { sendAndConfirmTransaction } from '@solana/web3.js';
+import { Connection, programs } from '@metaplex/js';
+const { metadata: { Metadata } } = programs;
 
 
 require('@solana/wallet-adapter-react-ui/styles.css');
@@ -23,72 +21,23 @@ function App() {
 
   async function click() {    
 
-    const TOKEN_AMOUNT = 2;
-
     const getBalance = async () => {
       console.log(adapter)
       const balance = await connection.getBalance(adapter.publicKey);
       console.log(balance)
     }
-    const   createVault = async () => {
-      const externalPriceAccountData = await  actions.createExternalPriceAccount({
-        connection,
-        wallet,
-      });
-      console.log(externalPriceAccountData)
-      await connection.confirmTransaction(externalPriceAccountData.txId, 'finalized');
 
-      const vault = await actions.createVault({
-        connection,
-        wallet,
-        ...externalPriceAccountData,
-      });
-      console.log(vault)
-
-      const testNfts = [];
-
-      for (let i = 0; i < TOKEN_AMOUNT; i++) {
-        const {
-          mint,
-          recipient: tokenAccount,
-          createAssociatedTokenAccountTx,
-          createMintTx,
-          mintToTx,
-        } = await actions.prepareTokenAccountAndMintTxs(connection, wallet.publicKey);
-  
-        await sendAndConfirmTransaction(
-          connection,
-          Transaction.fromCombined([createMintTx, createAssociatedTokenAccountTx, mintToTx]),
-          [ mint, wallet.publicKey],
-        );
-  
-        testNfts.push({
-          tokenAccount,
-          tokenMint: mint.publicKey,
-          amount: new BN(1),
-        });
-      }
-  
-
-      const { safetyDepositTokenStores } = await actions.addTokensToVault({
-        connection,
-        wallet,
-        vault,
-        nfts: testNfts,
-      });
-      console.log(safetyDepositTokenStores)
-
-      
-  
-      /*const vault = await Vault.load(connection, vaultResponse.vault);
-      console.log(vault) */
-    
+    const getMetadata = async () => {
+      console.log(adapter.publicKey)
+      const nftsmetadata = await Metadata.findDataByOwner(connection, adapter.publicKey);
+      console.log(nftsmetadata);
     }
+    
   
   
     if (wallet.connected) {
-      getBalance()
-      createVault()
+      getBalance() 
+      getMetadata()   
     }
   }
 
